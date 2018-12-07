897b93bd1c4254a8dcfd83942e7db81bdf822df7
sseyi <s.solanke.ss@gmail.com>
2018-12-07 11:17:42 -0500

    EnglishPremiere-DB-Front-End got signin & signup successful got all teams to showTeamResults with player data

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -32,11 +32,21 @@ const getTeamsSuccess = response => {
   $('#showTeamResults').html('')
   console.log(response)
   response.teams.forEach(function (team) {
+    let playerHTML = ''
+    team.players.forEach(function (player) {
+      playerHTML += (`
+        <h4>first_name/${player.first_name} </h4>
+      <p> last_name/${player.last_name} </p>
+        <p> player/${player.age} </p>
+      <p> position/${player.position} </p>
+      <p> goals/${player.goals} </p>
+    `)
+    })
     const teams = (`
       <h4> ${team.name} </h4>
-      <p> ${team.standings} </p>
+        <p> ${team.standings} </p>
       <p> ${team.location} </p>
-      <p> ${team.players} </p>
+      <p> ${playerHTML} </p>
     `)
     $('#showTeamResults').append(teams)
   })
